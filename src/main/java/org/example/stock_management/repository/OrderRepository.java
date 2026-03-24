50387caee2cf292e6bb7b39abe9544d4c31665b3
Chaitanya Garg <gargchaitanya22@gmail.com>
2026-03-24 11:59:57 +0530

    Revert "Added Stock Management Project"
    This reverts commit eba5f0137a10f0c4d226a93d458abf8968f98b6d.

--- src/main/java/org/example/stock_management/repository/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package org.example.stock_management.repository;
-
-import org.example.stock_management.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {}
